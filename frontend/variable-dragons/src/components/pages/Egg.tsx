b852874dad3c1733d9a65f4c940b1d67574197f9
morine131 <shu@shunoMacBook-Air.local>
2023-07-02 23:11:47 +0900

    pretty egg page layout

--- frontend/variable-dragons/src/components/pages/Egg.tsx
+++ frontend/variable-dragons/src/components/pages/Egg.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 import egg from "@/assets/egg.png";
 export const Egg = () => {
   return (
-    <Box>
+    <Box width="100%" display={"flex"} justifyContent={"center"}>
       <img src={egg}></img>
     </Box>
   );
