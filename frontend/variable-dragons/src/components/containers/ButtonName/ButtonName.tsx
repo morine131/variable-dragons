52c7ad861b2b558fd0add4e38be33d0484f82f3a
morine131 <shu@shunoMacBook-Air.local>
2023-07-09 00:11:01 +0900

    add name input and name button

---
+++ frontend/variable-dragons/src/components/containers/ButtonName/ButtonName.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export const ButtonName = () => {
+  return (
+    <Button variant="contained" color="secondary">
+      Name
+    </Button>
+  );
+};
--- frontend/variable-dragons/src/components/pages/Egg.tsx
+++ frontend/variable-dragons/src/components/pages/Egg.tsx
@@ -1,10 +1,22 @@
 import { Box } from "@mui/material";
 
-import egg from "@/assets/egg.png";
+import { ButtonName } from "../containers/ButtonName/ButtonName";
+import { InputName } from "../containers/InputName/InputName";
+import { EggImage } from "../presentations/EggImage";
+
 export const Egg = () => {
   return (
-    <Box width="100%" display={"flex"} justifyContent={"center"}>
-      <img src={egg}></img>
+    <Box
+      width="100%"
+      display={"flex"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <EggImage />
+      <InputName />
+      <Box mt={2}>
+        <ButtonName />
+      </Box>
     </Box>
   );
 };
